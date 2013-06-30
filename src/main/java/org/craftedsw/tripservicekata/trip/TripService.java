f468152544ca7f58452cbd590c0497db4d9b5778
Jimmy Cheng <chengnianhua@gmail.com>
2013-06-30 22:29:26 +0800

    call isFriends in TripService class, restructure the code

--- src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -7,38 +7,29 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
-
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = tripsBy(user);
-			}
-			return tripList;
-		} else {
+
+		if (getLoggedUser() == null) {
 			throw new UserNotLoggedInException();
 		}
+
+		return user.isFriendWith(getLoggedUser()) 
+				? tripsBy(user)
+			    : noTrips();
 	}
 
+	private ArrayList<Trip> noTrips() {
+		return new ArrayList<Trip>();
+	}
 
 	protected List<Trip> tripsBy(User user) {
 		return TripDAO.findTripsByUser(user);
 	}
-	
 
 	protected User getLoggedUser() {
 		return UserSession.getInstance().getLoggedUser();
 	}
-	
-	
+
 }
