69d45575e6d5b09432c94d460f331246258a729b
JimmyCheng <chengnianhua@gmail.com>
2013-06-30 15:53:08 +0800

    relocate the userbuilder

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -9,6 +9,7 @@ import org.craftedsw.tripservicekata.user.User;
 import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.Matchers.*;
+import static org.craftedsw.tripservicekata.trip.UserBuilder.*;
 
 public class TripServiceTest {
 
@@ -36,7 +37,7 @@ public class TripServiceTest {
 
 	@Test
 	public void should_not_return_any_friends_when_users_are_not_friends(){
-		User friend = UserBuilder.aUser()
+		User friend = aUser()
 				      .friendWith(ANOTHER_USER)
 				      .withTrips(TO_BRAZIL)
 				      .build();
@@ -50,7 +51,7 @@ public class TripServiceTest {
 	public void should_retrun_friend_trips_when_users_are_friends() throws Exception {
 		TripService tripService = new TestableTripService();
 
-		User friend = UserBuilder.aUser()
+		User friend = aUser()
 				      .friendWith(ANOTHER_USER, loggedInUser)
 				      .withTrips(TO_BRAZIL, TO_LONDON)
 				      .build();
@@ -60,47 +61,6 @@ public class TripServiceTest {
 		assertThat(friendTrips.size(),is(2));		
 	}
 	
-	public static class UserBuilder{
-
-		private User[] friends = new User[]{};
-		private Trip[] trips = new Trip[]{};
-
-		public static UserBuilder aUser(){
-			return new UserBuilder();
-		}
-
-		public User build() {
-            User user = new User();
-            addTripsTo(user);
-            addFriendsTo(user);
-            return user;
-		}
-
-		private void addFriendsTo(User user) {
-            for(Trip trip:trips){
-            	user.addTrip(trip);
-            }
-            
-		}
-
-		private void addTripsTo(User user) {
-			for(User friend:friends){
-				user.addFriend(friend);
-			}
-		}
-
-		public UserBuilder withTrips(Trip...trips) {
-            this.trips  = trips; 
-			return this;
-		}
-
-		public UserBuilder friendWith(User...friends) {
-            this.friends  = friends;
-			return this;
-		}
-	}
-
-
 	private class TestableTripService extends TripService {
 
 		@Override
